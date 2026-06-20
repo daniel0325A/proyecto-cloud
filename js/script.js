854af1749d02dc7367db9407135a1694834be132
daniel0325A <daniel03254101@gmail.com>
2026-06-19 22:22:45 -0500

    version_final_de_la_pagina

--- js/script.js
+++ js/script.js
@@ -47,3 +47,33 @@ cards.forEach(card => {
     });
 
 });
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        modal.style.display = "block";
+        modalImg.src = img.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
+
+modal.addEventListener("click", (e) => {
+
+    if(e.target === modal){
+
+        modal.style.display = "none";
+
+    }
+
+});
